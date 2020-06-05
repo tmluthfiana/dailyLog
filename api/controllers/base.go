3e7d5d980f09c9d8f47e9e586fd9953c4e009050
trias luthfiana <trias.luthfiana@outlook.com>
2020-06-05 16:24:10 +0700

    no message

--- api/controllers/base.go
+++ api/controllers/base.go
@@ -8,7 +8,6 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 
-	_ "github.com/jinzhu/gorm/dialects/mysql"    //mysql database driver
 	_ "github.com/jinzhu/gorm/dialects/postgres" //postgres database driver
 
 	"github.com/tmluthfiana/dailyLog/api/models"
